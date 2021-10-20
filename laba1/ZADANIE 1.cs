2e868586802481820f698a3d899185139d4448a6
Milashevsky Mykyta <milashevsky.nikita@gmail.com>
2021-10-20 13:34:08 +0300

    Лабораторна робота №1 виконана
    Лабораторна робота №1 виконана
    
    Co-Authored-By: Olga Sulema <73078541+olgasulema@users.noreply.github.com>
    Co-Authored-By: DmytroRvach <73078671+DmytroRvach@users.noreply.github.com>

---
+++ laba1/ZADANIE 1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static System.Math;
+using static System.Console;
+
+namespace ZADANIE_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                WriteLine("x = ");
+                double x = double.Parse(ReadLine());
+                WriteLine("y = ");
+                double y = double.Parse(ReadLine());
+                WriteLine("z = ");
+                double z = double.Parse(ReadLine());
+                double a, b;
+
+                if (z != 0)
+                {
+                    a = Cos((x + x * y) / z);
+                    if (Cos(a)!=0)
+                    {
+                        b = (x * x * x) / Cos(a);
+
+                        WriteLine(a);
+                        WriteLine(b);
+                    }
+                    else
+                    {
+                        WriteLine("Does not satisfy RAV");
+                    }
+                }
+                else
+                {
+                    WriteLine("z =/= 0");
+                }
+            }
+            catch
+            {
+                WriteLine("Incorrect data");
+            }
+        }
+    }
+}
---
+++ laba1/ZADANIE 2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using static System.Console;
+class ZADANIE_2
+{
+            
+    static void SieveOfEratosthenes(int n, bool[] prime)
+    {
+        try
+        {
+            for (int i = 0; i <= n; i++)
+                prime[i] = true;
+
+            for (int p = 2; p * p <= n; p++)
+            {
+                if (prime[p] == true)
+                {
+                    for (int i = p * 2; i <= n; i += p)
+                        prime[i] = false;
+                }
+            }
+        }
+        catch
+        {
+            WriteLine("Calculation Error");
+        }
+    }
+
+    static void MersennePrimes(int n)
+    {
+        try
+        {
+            bool[] prime = new bool[n + 1];
+
+            SieveOfEratosthenes(n, prime);
+
+            for (int k = 1; ((1 << k) - 1) <= n; k++)
+            {
+                long num = (1 << k) - 1;
+
+                if (prime[(int)(num)])
+                    Write(num + " ");
+            }
+        }
+        catch
+        {
+            WriteLine("Calculation Error");
+        }
+    }
+    public static void Main()
+    {
+        try
+        {
+           Write("n = ");
+           int n = int.Parse(ReadLine());
+            if (n <= 0)
+            {
+                WriteLine("n must be natural");
+            }
+            else
+            {
+                WriteLine("Mersenne prime numbers" + " smaller than or equal to " + n);
+
+                MersennePrimes(n);
+            }
+        }
+        catch
+        {
+            WriteLine("Input Error");
+        }
+        
+    }
+   
+}  
